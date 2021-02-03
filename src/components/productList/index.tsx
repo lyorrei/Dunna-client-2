5ab5fd84b573f75b20797892fdb3e56ebf2714b2
Lyorrei Shono Quintão <reishono@outlook.com>
2021-02-03 08:05:19 -0300

    Finished checkout page and started contact page

--- src/components/productList/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-interface Props {}
-
-const productList: React.FC<Props> = props => {
-    return <div>Product List</div>
-}
-
-export default productList
--- src/styles/pages/app.ts
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components'
-
-export const Content = styled.div`
-    padding-top: 6rem;
-    position: relative;
-`
